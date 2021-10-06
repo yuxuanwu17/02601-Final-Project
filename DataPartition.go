f5ddb31d71924865175ea5fdc2a6e2a58c23226b
yuxuanwu17 <yuxuan.wu17@gmail.com>
2021-10-06 19:46:58 -0400

    shuffle function achieved

--- DataPartition.go
+++ DataPartition.go
@@ -1,15 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"time"
+)
 
 /*
 This would separate the X and Y value into training (80%) and testing data (20%)
 */
 
-func DataPartition(X [][][]float64, Y []string) ([][][]float64, [][][]float64, []string, []string) {
-	fmt.Println(len(X))
-	//fmt.Println(Y)
+func DataPartition(X [][]float64, Y []string) ([][]float64, [][]float64, []string, []string) {
 
 	return nil, nil, nil, nil
+}
 
+func Shuffle(slice []int) {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for len(slice) > 0 {
+		n := len(slice)
+		randIndex := r.Intn(n)
+		slice[n-1], slice[randIndex] = slice[randIndex], slice[n-1]
+		slice = slice[:n-1]
+	}
 }
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	//ReadSingleFile("ass2_processed_data/A1.jpeg")
-	X, Y := ReadMultipleFiles("ass2_processed_data")
-	fmt.Println()
-	fmt.Println(len(X))
-	fmt.Println(len(X[0]))
-	fmt.Println(len(Y))
+	/*X's size is 2400*1152 */
+	//X, Y := ReadMultipleFiles("ass2_processed_data")
 	//x_train, x_test, y_train, y_test := DataPartition(X, Y)
+	//DataPartition(X, Y)
+	test := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for i := 0; i < 10; i++ {
+		Shuffle(test)
+		fmt.Println(test)
+		sort.Ints(test)
+		fmt.Println(test)
+		fmt.Println("====================")
+	}
+	//fmt.Println(len(X))
+	//fmt.Println(len(X[0]))
+	//fmt.Println(len(Y))
 	//fmt.Println(len(x_train))
 	//fmt.Println(len(x_test))
 	//fmt.Println(len(y_train))
