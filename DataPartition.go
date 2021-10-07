6f4f4efa4aad9a3db8850e1ac54a54e7e6cf2ea7
yuxuanwu17 <yuxuan.wu17@gmail.com>
2021-10-06 20:41:54 -0400

    successfully divide the dataset into two components

--- DataPartition.go
+++ DataPartition.go
@@ -9,9 +9,48 @@ import (
 This would separate the X and Y value into training (80%) and testing data (20%)
 */
 
-func DataPartition(X [][]float64, Y []string) ([][]float64, [][]float64, []string, []string) {
+func DataPartition(X [][]float64, Y []string, ratio float64) ([][]float64, [][]float64, []string, []string) {
 
-	return nil, nil, nil, nil
+	allDataNum := len(X)
+	trainNum := int(float64(allDataNum) * ratio)
+
+	allIndex := make([]int, 0)
+	for i := 0; i < len(X); i++ {
+		allIndex = append(allIndex, i)
+	}
+
+	// shuffle all the index
+	Shuffle(allIndex)
+
+	// divide into training and testing set based on the previous division
+	trainIndex := allIndex[:trainNum]
+	testIndex := allIndex[trainNum:]
+
+	X_train := make([][]float64, 0)
+	X_test := make([][]float64, 0)
+	y_train := make([]string, 0)
+	y_test := make([]string, 0)
+
+	for _, val := range trainIndex {
+		X_train = append(X_train, X[val])
+		y_train = append(y_train, Y[val])
+	}
+
+	for _, val := range testIndex {
+		X_test = append(X_test, X[val])
+		y_test = append(y_test, Y[val])
+	}
+
+	//fmt.Println(y_train)
+	//fmt.Println("==========================")
+	//fmt.Println(y_test)
+	//fmt.Println(X_train)
+	//fmt.Println(train_Index)
+	//fmt.Println(len(train_Index))
+	//fmt.Println(len(test_Index))
+	//fmt.Println(test_Index)
+
+	return X_train, X_test, y_train, y_test
 }
 
 func Shuffle(slice []int) {
--- main.go
+++ main.go
@@ -1,29 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
 func main() {
 	/*X's size is 2400*1152 */
-	//X, Y := ReadMultipleFiles("ass2_processed_data")
-	//x_train, x_test, y_train, y_test := DataPartition(X, Y)
-	//DataPartition(X, Y)
-	test := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	X, Y := ReadMultipleFiles("ass2_processed_data")
+	//x_train, x_test, y_train, y_test := DataPartition(X, Y,0.8)
+	DataPartition(X, Y, 0.8)
 
-	for i := 0; i < 10; i++ {
-		Shuffle(test)
-		fmt.Println(test)
-		sort.Ints(test)
-		fmt.Println(test)
-		fmt.Println("====================")
-	}
-	//fmt.Println(len(X))
-	//fmt.Println(len(X[0]))
-	//fmt.Println(len(Y))
-	//fmt.Println(len(x_train))
-	//fmt.Println(len(x_test))
-	//fmt.Println(len(y_train))
-	//fmt.Println(len(y_test))
 }
