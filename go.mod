354f6df76f489680b76edf54fd935ea6ecb7db01
yuxuanwu17 <yuxuan.wu17@gmail.com>
2021-10-09 08:20:21 -0400

    tensorflow 安装完成

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module finalProj4Prgram
 go 1.17
 
 require github.com/goml/gobrain v0.0.0-20201212123421-2e2d98ca8249
+
+require (
+	github.com/galeone/tensorflow/tensorflow/go v0.0.0-20210519172502-4018d721b591 // indirect
+	github.com/galeone/tfgo v0.0.0-20210914072119-caabdf830ee3 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,57 +2,39 @@ package main
 
 import (
 	"fmt"
-	"github.com/goml/gobrain"
-	"math/rand"
+	tf "github.com/galeone/tensorflow/tensorflow/go"
+	tg "github.com/galeone/tfgo"
 )
 
 func main() {
-	// set the random seed to 0
-	rand.Seed(0)
+	root := tg.NewRoot()
+	A := tg.NewTensor(root, tg.Const(root, [2][2]int32{{1, 2}, {-1, -2}}))
+	x := tg.NewTensor(root, tg.Const(root, [2][1]int64{{10}, {100}}))
+	b := tg.NewTensor(root, tg.Const(root, [2][1]int32{{-10}, {10}}))
+	Y := A.MatMul(x.Output).Add(b.Output)
+	// Please note that Y is just a pointer to A!
 
-	// create the XOR representation patter to train the network
-	/*X's size is 2400*1152 */
-	X, Y := ReadMultipleFiles("ass2_processed_data")
-	// ReadMultipleFiles("ass2_processed_data")
-	DataPartition(X, Y, 0.99)
-	//inputTrain, _, _, _, _, _ := DataPartition(X, Y, 0.99)
-	_, _, _, x_test, _, y_test := DataPartition(X, Y, 0.99)
-
-	fmt.Println(x_test)
-	fmt.Println(y_test)
-
-	fmt.Println(len(x_test))
-	fmt.Println(len(x_test[0]))
-	fmt.Println(len(y_test))
-	fmt.Println(len(y_test[0]))
-	//fmt.Println(inputTrain)
-
-	// patterns
-
-	patterns := [][][]float64{
-		{{0, 0}, {0}},
-		{{0, 1}, {1}},
-		{{1, 0}, {1}},
-		{{1, 1}, {0}},
-	}
-
-	//fmt.Println(patterns[0])
-	//fmt.Println(patterns[0][0])
-	//fmt.Println(patterns[0][1])
-	// instantiate the Feed Forward
-	ff := &gobrain.FeedForward{}
-
-	// initialize the Neural Network;
-	// the networks structure will contain:
-	// 2 inputs, 2 hidden nodes and 1 output.
-	ff.Init(2, 100, 1)
-
-	// train the network using the XOR patterns
-	// the training will run for 1000 epochs
-	// the learning rate is set to 0.6 and the momentum factor to 0.4
-	// use true in the last parameter to receive reports about the learning error
-	ff.Train(patterns, 1000, 0.6, 0.4, true)
-	//ff.Train(inputTrain, 1000, 0.6, 0.4, true)
+	// If we want to create a different node in the graph, we have to clone Y
+	// or equivalently A
+	Z := A.Clone()
+	results := tg.Exec(root, []tf.Output{Y.Output, Z.Output}, nil, &tf.SessionOptions{})
+	fmt.Println("Y: ", results[0].Value(), "Z: ", results[1].Value())
+	fmt.Println("Y == A", Y == A) // ==> true
+	fmt.Println("Z == A", Z == A) // ==> false
 }
+
+//func main() {
+//	// set the random seed to 0
+//	rand.Seed(0)
+//
+//	// create the XOR representation patter to train the network
+//	/*X's size is 2400*1152 */
+//	X, Y := ReadMultipleFiles("ass2_processed_data")
+//	// ReadMultipleFiles("ass2_processed_data")
+//	DataPartition(X, Y, 0.99)
+//	//inputTrain, _, _, _, _, _ := DataPartition(X, Y, 0.99)
+//	//_, _, _, x_test, _, y_test := DataPartition(X, Y, 0.99)
+//
+//}
 
 //DataPartition(X, Y, 0.8)
