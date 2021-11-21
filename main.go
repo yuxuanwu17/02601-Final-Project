8973f0bb18d3b3e292ad14aaf5f5f1a42ccc3f39
yuxuanwu17 <yuxuan.wu17@gmail.com>
2021-11-21 15:13:06 -0500

    1. fail to present the figure in html go file 2. plan to check the gotf

--- main.go
+++ main.go
@@ -29,6 +29,11 @@ func main() {
 	}
 
 	if *file != "" {
+		//http.HandleFunc("/predict", PredictSingleFigure)
+		//err := http.ListenAndServe(":9090", nil)
+		//if err != nil {
+		//	fmt.Printf("http serve failed, err:%v\n", err)
+		//}
 		fig := ReadSingleFile(*file)
 		load(&net)
 		fmt.Println("The predicted label is:", TokenToLabel(SingleImagePredict(&net, fig)))
@@ -36,3 +41,10 @@ func main() {
 	}
 
 }
+
+//func PredictSingleFigure(w http.ResponseWriter, r *http.Request) {
+//	//b ,_ :=ioutil.ReadFile("web/web.txt")
+//	_, _ = fmt.Fprintf(w, "<h1>Hello world</h1>\n" +
+//		"<button id= 'b1'>点我</button>\n"+
+//		"<img src='plot/python_cm.png'>")
+//}
